ba037493570496f3d9e485c4e54c07e9878d6288
Theresa Tran <theresa1194@gmail.com>
2023-11-04 17:22:51 +1100

    done

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Add your code here
+//fetch() request inside the submitData
+//submitdata returns the fetch()
+
+
+
+
+function submitData(name,email) {
+    let data = {
+        name: name,
+        email: email
+    };
+
+    let configData = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+    return fetch('http://localhost:3000/users', configData)      
+        .then(resp => resp.json())
+        .then(data => {
+            let id = data.id;
+            document.body.innerHTML += `<p> New user ID: ${id}</p>`;
+        })
+        .catch(error => {
+            document.body.innerHTML += `<p>Error: ${error.message}</p>`
+        });
+
+
+}
+
+submitData('Jane Doe', 'Jane.doe@email.com');
+
+
+
+
+
+
+
+
+
+    
